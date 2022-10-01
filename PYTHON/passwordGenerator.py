4c46964a1ea8590822b4b4844a13127c843b486a
lixa1o <93343432+lixa1o@users.noreply.github.com>
2022-10-01 20:30:33 +0530

    Update passwordGenerator.py

--- PYTHON/passwordGenerator.py
+++ PYTHON/passwordGenerator.py
@@ -18,7 +18,7 @@ def generate_random_password():
     password = []
     for i in range(length):
         password.append(random.choice(characters))
-
+        print(password)
     # shuffling the resultant password
     random.shuffle(password)
 
@@ -26,6 +26,7 @@ def generate_random_password():
     password = "".join(password)
     # printing the list
     print("Your strong password is :", password)
+    
 
 
 # invoking the function
