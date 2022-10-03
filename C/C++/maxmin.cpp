63bc70d8eab6e38cdbe9ca7a85c3c865d1b78f59
Aditya Pachauri <adityapachauri182003@gmail.com>
2022-10-03 23:54:19 +0530

    code to find max and min in an array

---
+++ C/C++/maxmin.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+using namespace std;
+
+int getMin(int num[], int n) {
+
+    int mini = INT_MAX;
+    
+    for(int i = 0; i<n; i++) {
+        
+        mini = min( mini, num[i]);
+        
+       
+    }
+
+  
+    return mini;
+}
+
+int getMax(int num[], int n) {
+
+    int maxi = INT_MIN;
+    
+    for(int i = 0; i<n; i++) {
+
+        maxi = max(maxi, num[i]);
+
+      
+    }
+
+  
+    return maxi;
+}
+
+int main() {
+
+    int size;
+    cin >> size;
+
+    int num[100];
+
+    //taking input in array
+    for(int i = 0; i<size; i++) {
+        cin >> num[i];
+    }
+
+    cout << " Maximum value is " << getMax(num, size) << endl;
+    cout << " Minimum value is " << getMin(num, size) << endl;
+
+
+    return 0;
+}
