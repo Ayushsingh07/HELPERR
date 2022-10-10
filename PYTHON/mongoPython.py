8277410a7599c634aead32e2fc2c04445a72d7a9
Akash Kamerkar <55013624+akash9579@users.noreply.github.com>
2022-10-10 18:43:13 +0530

    Add files via upload
    Added python file which connect mongodb

---
+++ PYTHON/mongoPython.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 10 18:10:23 2022
+
+@author: INAKKAM
+"""
+
+from pymongo import MongoClient
+
+def mongoConnect():
+    client = MongoClient();
+    print("Connection Successful")
+    client.close()
+
+mongoConnect()
