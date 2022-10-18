1aa8dad5c1a8605e25be536b5edfb556046a5568
Prince Piyush <98477288+princepiyush07@users.noreply.github.com>
2022-10-18 21:59:41 +0530

    Adding BucketSort

--- C/C++/bucketsort.cpp
+++ C/C++/bucketsort.cpp
@@ -7,31 +7,29 @@ using namespace std;
 void bucketSort(float arr[], int n)
 {
 	
-	vector<float> b[n];
+	vector<float> t[n];
 
 	for (int i = 0; i < n; i++) {
 		int bi = n * arr[i]; 
-		b[bi].push_back(arr[i]);
+		t[bi].push_back(arr[i]);
 	}
 
 	
 	for (int i = 0; i < n; i++)
-		sort(b[i].begin(), b[i].end());
+		sort(t[i].begin(), t[i].end());
 
 	int index = 0;
 	for (int i = 0; i < n; i++)
-		for (int j = 0; j < b[i].size(); j++)
-			arr[index++] = b[i][j];
+		for (int j = 0; j < t[i].size(); j++)
+			arr[index++] = t[i][j];
 }
 
 
 int main()
 {
-	float arr[]
-		= { 0.897, 0.565, 0.656, 0.1234, 0.665, 0.3434 };
+	float arr[]= { 0.897, 0.565, 0.656, 0.1234, 0.665, 0.3434 };
 	int n = sizeof(arr) / sizeof(arr[0]);
 	bucketSort(arr, n);
-
 	cout << "Sorted array is \n";
 	for (int i = 0; i < n; i++)
 		cout << arr[i] << " ";
