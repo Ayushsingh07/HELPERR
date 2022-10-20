e3b669343a553ebf0c4ee6b7b9a1503874883667
Soumya Sharma <soumya10072001@gmail.com>
2022-10-20 20:29:46 +0530

    Target SUm DP.

--- C/C++/TargetSum_DP.cpp
+++ C/C++/TargetSum_DP.cpp
@@ -6,10 +6,10 @@ int findWays(vector<int> &num, int tar){
 
     vector<int> prev(tar+1,0);
     
-    if(num[0] == 0) prev[0] =2;  // 2 cases -pick and not pick
-    else prev[0] = 1;  // 1 case - not pick
+    if(num[0] == 0) prev[0] =2;  
+    else prev[0] = 1;  
     
-    if(num[0]!=0 && num[0]<=tar) prev[num[0]] = 1;  // 1 case -pick
+    if(num[0]!=0 && num[0]<=tar) prev[num[0]] = 1;  
     
     for(int ind = 1; ind<n; ind++){
         vector<int> cur(tar+1,0);
